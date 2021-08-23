8284a2911e18650082efd3cd907d8a476bb523da
Anshuman Pandey <anshumanp70@gmail.com>
2021-08-23 11:43:59 +0530

    use_data to return reguest

--- main.py
+++ main.py
@@ -2,21 +2,27 @@
 comment add for checking
 you use this repository form another place  
 this is very important
+
 '''
 
 from flask import Flask
 from flask_restful import Api, Resource
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data":"HelloWorld"}
-    def post(self):
-        return {"data":"HelloWorld"}
+names = {}
 
-api.add_resource(HelloWorld,"/helloworld")
+class HelloWorld(Resource):
+    def get(self, name, mode):
+        if mode:
+            names[name] = "www.google.com/" + name
+            return {"message":"your data has been saved"}
+        else:
+            return {"details": names[name]}
+
+api.add_resource(HelloWorld,"/helloworld/<string:name>/<int:mode>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-resposnse = requests.post(BASE+"helloworld")
+resposnse = requests.post(BASE+"helloworld/Anshuman/1")
 print(resposnse.json())
-resposnse = requests.get(BASE+"helloworld")
+resposnse = requests.get(BASE+"helloworld/Anshuman/0")
 print(resposnse.json())
