7e99b48bcc39c4bf6f0a2a6d822d5929cc39ea5c
Anshuman Pandey <anshumanp70@gmail.com>
2021-08-26 09:36:04 +0530

    delete method added

--- main.py
+++ main.py
@@ -6,23 +6,40 @@ this is very important
 '''
 
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 import requests
+from requests.api import delete
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the vedio not given", required=True)
+video_put_args.add_argument("likes", type=int, help="likes on the vedio not given", required=True)
+video_put_args.add_argument("views", type=int, help="views of the vedio not given", required=True)
+videos = {}
 
-class HelloWorld(Resource):
-    def get(self, name, mode):
-        if mode:
-            names[name] = "www.google.com/" + name
-            return {"message":"your data has been saved"}
-        else:
-            return {"details": names[name]}
+class Video(Resource):
+    def get(self, video_id):
+        try:
+            return {video_id:videos[video_id]}
+        except:
+            return {"msg":"id is not present"}
+    
+    def put(Self, video_id):
+        if video_id in videos:
+            return {'msg':'this video_id already exist'}
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return {'msg':'video has been added'}
 
-api.add_resource(HelloWorld,"/helloworld/<string:name>/<int:mode>")
+    def delete(self, video_id):
+        if video_id not in videos:
+            return {'msg':'video_id is not present'}
+        videos.pop(video_id)
+        return {'msg': 'video_id ' + str(video_id) + ' has been deleted'}
+    
+api.add_resource(Video,"/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,7 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-resposnse = requests.post(BASE+"helloworld/Anshuman/1")
+resposnse = requests.put(BASE+"video/1",{'name':'at my worst','likes':100,'views':500000})
 print(resposnse.json())
-resposnse = requests.get(BASE+"helloworld/Anshuman/0")
+
+resposnse = requests.get(BASE+"video/1")
+print(resposnse.json())
+
+resposnse = requests.delete(BASE+"video/2")
+print(resposnse.json())
+
+resposnse = requests.delete(BASE+"video/1")
 print(resposnse.json())
