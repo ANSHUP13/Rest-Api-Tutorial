b6e13b5561fd06b26a65d8617f13fac38078416d
Anshuman Pandey <anshumanp70@gmail.com>
2021-08-26 10:55:33 +0530

    database  added

--- main.py
+++ main.py
@@ -5,39 +5,64 @@ this is very important
 
 '''
 
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-import requests
+from flask import Flask, views
+from flask_restful import Api, Resource, marshal_with, reqparse, fields
 from requests.api import delete
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100),nullable=False)
+    views = db.Column(db.Integer,nullable=False)
+    likes = db.Column(db.Integer,nullable=False)
+    
+    def __repr__(self) -> str:
+        return f"Video(name={name},views={views},likes={likes})"
+
+db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the vedio not given", required=True)
 video_put_args.add_argument("likes", type=int, help="likes on the vedio not given", required=True)
 video_put_args.add_argument("views", type=int, help="views of the vedio not given", required=True)
-videos = {}
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
+
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        try:
-            return {video_id:videos[video_id]}
-        except:
-            return {"msg":"id is not present"}
+        result = VideoModel.query.filter_by(id=video_id).first()
+        return result
     
+    @marshal_with(resource_fields)
     def put(Self, video_id):
-        if video_id in videos:
-            return {'msg':'this video_id already exist'}
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return {'msg':'video has been added'}
+        video = VideoModel(
+            id=video_id,
+            name=args['name'],
+            views=args['views'],
+            likes=args['likes']
+            )
+        db.session.add(video)
+        db.session.commit()
+        return video,201
 
-    def delete(self, video_id):
-        if video_id not in videos:
-            return {'msg':'video_id is not present'}
-        videos.pop(video_id)
-        return {'msg': 'video_id ' + str(video_id) + ' has been deleted'}
+#    def delete(self, video_id):
+#        if video_id not in videos:
+#            return {'msg':'video_id is not present'}
+#        videos.pop(video_id)
+#        return {'msg': 'video_id ' + str(video_id) + ' has been deleted'}
     
 api.add_resource(Video,"/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -2,14 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-resposnse = requests.put(BASE+"video/1",{'name':'at my worst','likes':100,'views':500000})
+resposnse = requests.put(BASE+"video/3",{'name':'at my worst','likes':100,'views':500000})
 print(resposnse.json())
 
-resposnse = requests.get(BASE+"video/1")
+resposnse = requests.get(BASE+"video/3")
 print(resposnse.json())
 
-resposnse = requests.delete(BASE+"video/2")
-print(resposnse.json())
-
-resposnse = requests.delete(BASE+"video/1")
-print(resposnse.json())
+#resposnse = requests.delete(BASE+"video/2")
+#print(resposnse.json())
+#
+#resposnse = requests.delete(BASE+"video/1")
+#print(resposnse.json())
